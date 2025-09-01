48a3d9af0b83985e2e9af605eef33de5c37cbf4c
Adebayo Shola <sholaadedev@gmail.com>
2025-09-01 21:27:06 +0100

    feat: add tenant access control to media and fix link bug

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -56,6 +56,7 @@ export default buildConfig({
     multiTenantPlugin({
       collections: {
         products: {},
+        media: {},
       },
       tenantsArrayField: {
         includeDefaultField: false,
